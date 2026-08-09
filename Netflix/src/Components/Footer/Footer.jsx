f9998ac34e789816046821d918bc5ec1b9955325
Dba17 <dbellaxomarkon@gmail.com>
2026-08-08 21:33:16 -0300

    Footer

--- Netflix/src/Components/Footer/Footer.jsx
+++ Netflix/src/Components/Footer/Footer.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { FaYoutube } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <FaInstagram />
+          <CiFacebook />
+          <FaYoutube />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notices</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_code">
+                <p>
+                    Service Code
+                </p>
+            </div>
+            <div className="copyright">
+              &copy; 1997-2026 Netflix, Inc.  
+            </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
